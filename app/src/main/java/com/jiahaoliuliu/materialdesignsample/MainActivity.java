09552e993695befe2a2aee6960efaffd67b6173f
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2015-06-06 00:20:55 +0400

    Improved the way to handle the drawer toggle

--- app/src/main/java/com/jiahaoliuliu/materialdesignsample/MainActivity.java
+++ app/src/main/java/com/jiahaoliuliu/materialdesignsample/MainActivity.java
@@ -43,18 +43,30 @@ public class MainActivity extends AppCompatActivity {
         mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch(item.getItemId()) {
+//            case android.R.id.home:
+//                if (mDrawerLayout.isDrawerOpen(mDrawer)) {
+//                    mDrawerLayout.closeDrawer(mDrawer);
+//                } else {
+//                    mDrawerLayout.openDrawer(mDrawer);
+//                }
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()) {
-            case android.R.id.home:
-                if (mDrawerLayout.isDrawerOpen(mDrawer)) {
-                    mDrawerLayout.closeDrawer(mDrawer);
-                } else {
-                    mDrawerLayout.openDrawer(mDrawer);
-                }
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
         }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
     }
 }
