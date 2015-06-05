cb1209958639597f0d91b8d6e0b77460ee01adc7
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2015-06-06 00:17:34 +0400

    Set listener to the drawer toggler to open or close the drawer

--- app/src/main/java/com/jiahaoliuliu/materialdesignsample/MainActivity.java
+++ app/src/main/java/com/jiahaoliuliu/materialdesignsample/MainActivity.java
@@ -1,15 +1,18 @@
 package com.jiahaoliuliu.materialdesignsample;
 
 import android.content.res.Configuration;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
+    private NavigationView mDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawer = (NavigationView) findViewById(R.id.drawer);
+
         mDrawerToggle = new ActionBarDrawerToggle(
                 this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close
             );
@@ -36,5 +41,20 @@ public class MainActivity extends AppCompatActivity {
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                if (mDrawerLayout.isDrawerOpen(mDrawer)) {
+                    mDrawerLayout.closeDrawer(mDrawer);
+                } else {
+                    mDrawerLayout.openDrawer(mDrawer);
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
