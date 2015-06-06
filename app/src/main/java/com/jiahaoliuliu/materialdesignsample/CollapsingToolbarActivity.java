dd965bcd50f1e2e95ce8b55b826b11d66a526030
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2015-06-06 21:46:02 +0400

    Using recycler view in the collapsing toolbar activity

--- app/src/main/java/com/jiahaoliuliu/materialdesignsample/CollapsingToolbarActivity.java
+++ app/src/main/java/com/jiahaoliuliu/materialdesignsample/CollapsingToolbarActivity.java
@@ -2,6 +2,7 @@ package com.jiahaoliuliu.materialdesignsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.RecyclerView;
 
@@ -14,7 +15,7 @@ public class CollapsingToolbarActivity extends AppCompatActivity {
     private String[] mRecyclerViewValues =
             new String[]{"Apple Pie", "Banana Bread", "Cupcake", "Donut", "Eclair", "Froyo",
                     "Gingerbread", "Honeycomb", "Ice Cream Sandwich", "Jelly Bean", "KitKat",
-                    "Lollipop", "M preview", "N", "O", "P", "Q", "R", "S", "T"};
+                    "Lollipop", "M preview"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +27,18 @@ public class CollapsingToolbarActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-//        // Link the views
-//        mSimpleRecyclerView = (RecyclerView) findViewById(R.id.simpleRecyclerView);
-//
-//        // Use this setting to improve performance if you know that changes
-//        // in content do not change the layout size of the RecyclerView
-//        mSimpleRecyclerView.setHasFixedSize(true);
-//
-//        // Use a linear layout manager
-//        mLayoutManager = new LinearLayoutManager(this);
-//        mSimpleRecyclerView.setLayoutManager(mLayoutManager);
-//
-//        SimpleRecyclerViewAdapter mSimpleRecyclerViewAdapter = new SimpleRecyclerViewAdapter(mRecyclerViewValues);
-//        mSimpleRecyclerView.setAdapter(mSimpleRecyclerViewAdapter);
+        // Link the views
+        mSimpleRecyclerView = (RecyclerView) findViewById(R.id.simpleRecyclerView);
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mSimpleRecyclerView.setHasFixedSize(true);
+
+        // Use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mSimpleRecyclerView.setLayoutManager(mLayoutManager);
+
+        SimpleRecyclerViewAdapter mSimpleRecyclerViewAdapter = new SimpleRecyclerViewAdapter(mRecyclerViewValues);
+        mSimpleRecyclerView.setAdapter(mSimpleRecyclerViewAdapter);
     }
 }
