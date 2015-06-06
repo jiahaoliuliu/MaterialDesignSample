6f97e10a1f6712e0c00d6bdcb27c8a921aff2e46
Jiahao Liu Liu <jiahaoliuliu@gmail.com>
2015-06-06 21:00:44 +0400

    Set correctly a specific theme for Collapsing toolbar activity

--- app/src/main/java/com/jiahaoliuliu/materialdesignsample/CollapsingToolbarActivity.java
+++ app/src/main/java/com/jiahaoliuliu/materialdesignsample/CollapsingToolbarActivity.java
@@ -1,5 +1,7 @@
 package com.jiahaoliuliu.materialdesignsample;
 
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -7,7 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.widget.ArrayAdapter;
 import android.support.v7.widget.RecyclerView;
 
-public class CollapsingToolbarActivity extends AppCompatActivity {
+public class CollapsingToolbarActivity extends ActionBarActivity {
 
     private RecyclerView mSimpleRecyclerView;
     private RecyclerView.Adapter mAdapter;
